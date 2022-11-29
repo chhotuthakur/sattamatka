2837deff6eaffbe9fd3b31753cd446d82113058b
chhotuthakur <npsdahiya@gmail.com>
2022-11-29 22:37:36 +0530

    added two layout and read todo for more

--- app/src/main/java/com/rkonline/android/GhantaBazar.java
+++ app/src/main/java/com/rkonline/android/GhantaBazar.java
@@ -1,14 +1,21 @@
 package com.rkonline.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class GhantaBazar extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ghanta_bazar);
+
+//TODO: add place bet option and it will done with three more activities;
+
+
     }
 }
